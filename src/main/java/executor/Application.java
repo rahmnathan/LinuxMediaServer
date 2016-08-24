b22b85a4d3c231512e648ed3dd42c32a4eaabfdc
rahmnathan <rahm.nathan@gmail.com>
2016-08-23 19:25:08 -0500

    Bug fix

--- src/main/java/executor/Application.java
+++ src/main/java/executor/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("player")
+@ComponentScan(basePackages = {"player","executor"})
 public class Application {
 
     public static void main(String[] args) {
