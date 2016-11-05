50bc6c4f96e09df973169436fdd354247a963390
rahmnathan <rahm.nathan@gmail.com>
2016-11-05 13:38:36 -0500

    Refactoring

--- RestServer/src/main/java/nr/localmovies/restserver/RestListener.java
+++ RestServer/src/main/java/nr/localmovies/restserver/RestListener.java
@@ -100,7 +100,8 @@ public class RestListener {
     public void streamVideo(@RequestParam("seek") long seek,
                             HttpServletResponse response) throws Exception {
         InputStream is = new DataInputStream(new FileInputStream(video));
-        is.skip(seek);
+        if(seek != 0)
+            is.skip(seek);
         long totalLength = video.length();
         int bufferSize = 4000;
         response.setContentType("video/mp4");
