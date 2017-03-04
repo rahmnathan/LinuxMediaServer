a1bdf1ac2b18e1d4f7205c1b7505ea3400f5490c
rahmnathan <rahm.nathan@gmail.com>
2017-03-04 15:05:44 -0600

    Added image scaling to improve performance

--- local-movie-info-omdb/src/main/java/nr/localmovies/omdbmovieinfoprovider/OMDBIMovieInfoProvider.java
+++ local-movie-info-omdb/src/main/java/nr/localmovies/omdbmovieinfoprovider/OMDBIMovieInfoProvider.java
@@ -2,10 +2,15 @@ package nr.localmovies.omdbmovieinfoprovider;
 
 import nr.localmovies.movieinfoapi.MovieInfo;
 import nr.localmovies.movieinfoapi.IMovieInfoProvider;
+import org.imgscalr.Scalr;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,10 +38,22 @@ public class OMDBIMovieInfoProvider implements IMovieInfoProvider {
         try {
             URL url = new URL(jsonMovieInfo.get("Poster").toString());
             poster = dataProvider.getImage(url);
+            poster = scaleImage(poster);
         }catch (Exception e){
             logger.log(Level.WARNING, "Unable to get poster for movie - " + title);
         }
 
         return movieInfoMapper.mapOmdbJsonToMovieInfo(jsonMovieInfo, poster, fileName);
+    }
+
+    private byte[] scaleImage(byte[] poster) throws Exception {
+        BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(poster));
+        bufferedImage = Scalr.resize(bufferedImage, Scalr.Method.ULTRA_QUALITY, 300);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpg", outputStream);
+        outputStream.flush();
+        byte[] resizedPoster = outputStream.toByteArray();
+        outputStream.close();
+        return resizedPoster;
     }
 }
