999eaa7fd01ab72d6aa33cee2b9e4da4ca5e6735
rahmnathan <rahm.nathan@gmail.com>
2017-03-25 23:24:23 -0500

    Refactoring

--- local-movie-web/src/main/java/nr/localmovies/web/MultipartFileSender.java
+++ local-movie-web/src/main/java/nr/localmovies/web/MultipartFileSender.java
@@ -19,24 +19,18 @@ class MultipartFileSender {
         }
 
         Long length = Files.size(filepath);
-        Range full = new Range(0, length - 1, length);
-        String range = request.getHeader("Range");
+        Range range = new Range(0, length - 1, length);
+        String rangeHeader = request.getHeader("Range");
         InputStream input = new BufferedInputStream(Files.newInputStream(filepath));
         OutputStream output = response.getOutputStream();
-        if(range == null) {
-            System.out.println("null range");
-            response.setHeader("Content-Range", "bytes " + full.start + "-" + full.end + "/" + full.total);
-            response.setHeader("Content-Length", String.valueOf(full.length));
-            Range.copy(input, output, length, full.start, full.length);
-        } else {
-            long start = Long.valueOf(range.substring(6, range.length()-1));
-            Range range1 = new Range(start, length - 1, length);
-            System.out.println(start);
-            response.setHeader("Content-Range", "bytes " + range1.start + "-" + range1.end + "/" + range1.total);
-            response.setHeader("Content-Length", String.valueOf(range1.length));
+        if(rangeHeader != null) {
+            long start = Long.valueOf(rangeHeader.substring(6, rangeHeader.length()-1));
+            range = new Range(start, length - 1, length);
             response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-            Range.copy(input, output, length, range1.start, range1.length);
         }
+        response.setHeader("Content-Range", "bytes " + range.start + "-" + range.end + "/" + range.total);
+        response.setHeader("Content-Length", String.valueOf(range.length));
+        Range.copy(input, output, length, range.start, range.length);
     }
 
     private static class Range {
@@ -63,7 +57,6 @@ class MultipartFileSender {
             int read;
 
             if (inputSize == length) {
-                // Write full range.
                 while ((read = input.read(buffer)) > 0) {
                     output.write(buffer, 0, read);
                     output.flush();
