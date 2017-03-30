c0036274c0800a938ae92b6c7ab4bbde0342666d
rahmnathan <rahm.nathan@gmail.com>
2017-03-29 22:00:55 -0500

    Bug fixes

--- local-movie-service/src/main/java/nr/localmovies/control/DirectoryMonitor.java
+++ local-movie-service/src/main/java/nr/localmovies/control/DirectoryMonitor.java
@@ -13,6 +13,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
@@ -22,6 +24,7 @@ import javax.annotation.PreDestroy;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 @Service
 public class DirectoryMonitor {
@@ -56,18 +59,24 @@ public class DirectoryMonitor {
     private void startRecursiveWatcher() throws IOException {
         logger.info("Starting Recursive Watcher");
 
-        Consumer<Path> register = path -> {
+        final Map<WatchKey, Path> keys = new HashMap<>();
+
+        Consumer<Path> register = p -> {
+            if (!p.toFile().exists() || !p.toFile().isDirectory()) {
+                throw new RuntimeException("folder " + p + " does not exist or is not a directory");
+            }
             try {
-                Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                Files.walkFileTree(p, new SimpleFileVisitor<Path>() {
                     @Override
                     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                         logger.info("registering " + dir + " in watcher service");
-                        dir.register(watcher, new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_DELETE}, SensitivityWatchEventModifier.HIGH);
+                        WatchKey watchKey = dir.register(watcher, new WatchEvent.Kind[]{ENTRY_CREATE}, SensitivityWatchEventModifier.HIGH);
+                        keys.put(watchKey, dir);
                         return FileVisitResult.CONTINUE;
                     }
                 });
             } catch (IOException e) {
-                logger.info("Error registering path " + path);
+                throw new RuntimeException("Error registering path " + p);
             }
         };
 
@@ -75,14 +84,37 @@ public class DirectoryMonitor {
 
         executor.submit(() -> {
             while (true) {
+                final WatchKey key;
                 try {
-                    WatchKey key = watcher.take(); // wait for a key to be available
-                    key.reset();
+                    key = watcher.take(); // wait for a key to be available
                 } catch (InterruptedException ex) {
-                    logger.error("Directory watcher interrupted");
+                    return;
                 }
 
+                final Path dir = keys.get(key);
+                if (dir == null) {
+                    System.err.println("WatchKey " + key + " not recognized!");
+                    continue;
+                }
+
+                key.pollEvents().stream()
+                        .filter(e -> (e.kind() != OVERFLOW))
+                        .map(e -> ((WatchEvent<Path>) e).context())
+                        .forEach(p -> {
+                            final Path absPath = dir.resolve(p);
+                            if (absPath.toFile().isDirectory()) {
+                                register.accept(absPath);
+                            } else {
+                                final File f = absPath.toFile();
+                                logger.info("Detected new file " + f.getAbsolutePath());
+                            }
+                        });
+
                 fileListProvider.purgeTitleCache();
+                boolean valid = key.reset(); // IMPORTANT: The key must be reset after processed
+                if (!valid) {
+                    break;
+                }
             }
         });
     }
