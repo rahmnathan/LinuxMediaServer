b91a70137c3f8a61723a00a58745898dba6d8fd4
Nathan <rahm.nathan@gmail.com>
2017-04-26 18:49:44 -0500

    Refactoring

--- local-movie-directory-monitor/src/main/java/nr/localmovies/directory/monitor/DirectoryMonitor.java
+++ local-movie-directory-monitor/src/main/java/nr/localmovies/directory/monitor/DirectoryMonitor.java
@@ -33,6 +33,7 @@ public class DirectoryMonitor {
     private ExecutorService executor;
     private List<DirectoryMonitorObserver> observerList;
     private final Map<WatchKey, Path> keys = new HashMap<>();
+    private WatchService watchService;
 
     private void notifyObservers(){
         observerList.forEach(DirectoryMonitorObserver::directoryModified);
@@ -41,6 +42,7 @@ public class DirectoryMonitor {
     @Autowired
     public DirectoryMonitor(List<DirectoryMonitorObserver> observerList) {
         this.observerList = observerList;
+        this.executor = Executors.newSingleThreadExecutor();
     }
 
     @PreDestroy
@@ -51,58 +53,60 @@ public class DirectoryMonitor {
     @SuppressWarnings("unchecked")
     public void startRecursiveWatcher(String pathToMonitor) {
         logger.info("Starting Recursive Watcher");
-        executor = Executors.newSingleThreadExecutor();
-
-        try (WatchService watcher = FileSystems.getDefault().newWatchService()) {
-            Consumer<Path> register = p -> {
-                try {
-                    Files.walkFileTree(p, new SimpleFileVisitor<Path>() {
-                        @Override
-                        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                            logger.info("registering " + dir + " in watcher service");
-                            WatchKey watchKey = dir.register(watcher, new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_DELETE}, SensitivityWatchEventModifier.HIGH);
-                            keys.put(watchKey, dir);
-                            return FileVisitResult.CONTINUE;
-                        }
-                    });
-                } catch (IOException e) {
-                    logger.error(e.toString());
-                }
-            };
-
-            register.accept(Paths.get(pathToMonitor));
-
-            executor.submit(() -> {
-                while (true) {
-                    final WatchKey key;
-                    try {
-                        key = watcher.take();
-                    } catch (InterruptedException ex) {
-                        logger.error(ex.toString());
-                        continue;
-                    }
-
-                    final Path dir = keys.get(key);
-
-                    key.pollEvents().stream()
-                            .map(e -> ((WatchEvent<Path>) e))
-                            .forEach(p -> {
-                                if (!p.kind().equals(OVERFLOW)) {
-                                    final Path absPath = dir.resolve(p.context());
-                                    if (absPath.toFile().isDirectory()) {
-                                        register.accept(absPath);
-                                    } else {
-                                        logger.info("Detected new file " + absPath.toString());
-                                    }
-                                }
-                            });
-
-                    notifyObservers();
-                    key.reset();
-                }
-            });
+        
+        try{
+            this.watchService = FileSystems.getDefault().newWatchService();
         } catch (IOException e){
             logger.error(e.toString());
+            return;
         }
+
+        Consumer<Path> register = p -> {
+            try {
+                Files.walkFileTree(p, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        logger.info("registering " + dir + " in watcher service");
+                        WatchKey watchKey = dir.register(watchService, new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_DELETE}, SensitivityWatchEventModifier.HIGH);
+                        keys.put(watchKey, dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                logger.error(e.toString());
+            }
+        };
+
+        register.accept(Paths.get(pathToMonitor));
+
+        executor.submit(() -> {
+            while (true) {
+                final WatchKey key;
+                try {
+                    key = watchService.take();
+                } catch (InterruptedException ex) {
+                    logger.error(ex.toString());
+                    continue;
+                }
+
+                final Path dir = keys.get(key);
+
+                key.pollEvents().stream()
+                        .map(e -> ((WatchEvent<Path>) e))
+                        .forEach(p -> {
+                            if (!p.kind().equals(OVERFLOW)) {
+                                final Path absPath = dir.resolve(p.context());
+                                if (absPath.toFile().isDirectory()) {
+                                    register.accept(absPath);
+                                } else {
+                                    logger.info("Detected new file " + absPath.toString());
+                                }
+                            }
+                        });
+
+                notifyObservers();
+                key.reset();
+            }
+        });
     }
 }
