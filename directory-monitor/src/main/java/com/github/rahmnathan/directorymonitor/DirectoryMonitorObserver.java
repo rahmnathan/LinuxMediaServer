4b537857b5a537231c1ef4bc367060a40ef2dc84
nathan <rahm.nathan@gmail.com>
2017-08-24 21:55:37 -0500

    Moved directory monitor to it's own repository

--- directory-monitor/src/main/java/com/github/rahmnathan/directorymonitor/DirectoryMonitorObserver.java
+++
@@ -1,8 +0,0 @@
-package com.github.rahmnathan.directorymonitor;
-
-import java.nio.file.Path;
-import java.nio.file.WatchEvent;
-
-public interface DirectoryMonitorObserver {
-    void directoryModified(WatchEvent event, Path absolutePath);
-}
