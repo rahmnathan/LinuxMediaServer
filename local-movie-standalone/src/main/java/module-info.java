e55375e67b684419654011ca2886f2594ce3de46
nathan <rahm.nathan@gmail.com>
2018-05-15 21:23:39 -0500

    Added initial Hashicorp Vault integration logic

--- local-movie-standalone/src/main/java/module-info.java
+++ local-movie-standalone/src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module local.movie.standalone {
+    exports com.github.rahmnathan.localmovies.application.config;
     requires spring.boot.autoconfigure;
     requires spring.data.jpa;
     requires spring.context;
     requires spring.boot;
+    requires slf4j.api;
+    requires spring.vault.core;
+    requires spring.cloud.vault.config;
+    requires jackson.annotations;
 }
