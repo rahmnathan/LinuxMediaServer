b8fc149710ebaaaa360db33a46f3273f28fa2ad3
nathan <rahm.nathan@gmail.com>
2018-08-04 22:51:48 -0500

    Added metric for monitoring active video streams

--- localmovie-web/src/main/java/com/github/rahmnathan/localmovies/web/control/FileSender.java
+++ localmovie-web/src/main/java/com/github/rahmnathan/localmovies/web/control/FileSender.java
@@ -1,5 +1,6 @@
 package com.github.rahmnathan.localmovies.web.control;
 
+import io.micrometer.core.instrument.Metrics;
 import org.apache.http.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FileSender {
 
     private final Logger logger = LoggerFactory.getLogger(FileSender.class.getName());
+    private final AtomicInteger activeStreamGauge = Metrics.gauge("localmovies.stream.active", new AtomicInteger(0));
     private static final int DEFAULT_BUFFER_SIZE = 16384;
 
     public void serveResource(Path file, HttpServletRequest request, HttpServletResponse response) {
@@ -43,6 +46,8 @@ public class FileSender {
         try (InputStream input = new BufferedInputStream(Files.newInputStream(file));
              OutputStream output = response.getOutputStream()) {
 
+            activeStreamGauge.getAndIncrement();
+
             byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
             input.skip(startByte);
             while ((input.read(buffer)) > 0) {
@@ -52,6 +57,8 @@ public class FileSender {
 
         } catch (IOException e) {
             logger.error("Failure streaming video", e);
+        } finally {
+            activeStreamGauge.getAndDecrement();
         }
     }
 }
